40aee2e6bf3ba9374e6f9626125a3a73319a2f19
Don Yin <Don_Yin@outlook.com>
2022-11-05 21:21:03 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,9 +5,12 @@ echo "\
 email = \"don_yin@outlook.com\"\n\
 " > ~/.streamlit/credentials.toml
 
-# echo "\
-# [server]\n\
-# headless = true\n\
-# enableCORS=false\n\
-# port = $PORT\n\
-# " > ~/.streamlit/config.toml
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+[theme]
+base="dark"
+primaryColor="purple"
+" > ~/.streamlit/config.toml
